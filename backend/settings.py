39e81df7cc103a6aa10362ee0532196e072626bf
ayushsaraf <ayushsaraf.saraf15@gmail.com>
2023-09-14 17:58:30 +0530

    setings

--- backend/settings.py
+++ backend/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "resume",
+    "resume.apps.ResumeConfig",
     "rest_framework",
     "corsheaders",
     "ckeditor",

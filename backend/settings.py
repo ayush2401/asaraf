01213396042465fa02093ceb317742d7a1727258
ayushsaraf <ayushsaraf.saraf15@gmail.com>
2023-09-14 14:04:23 +0530

    sets

--- backend/settings.py
+++ backend/settings.py
@@ -42,11 +42,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "resume.apps.ResumeConfig",
     "rest_framework",
     "corsheaders",
     "ckeditor",
-    "tinymce",
-    "resume.config.ResumeConfig",
+    "tinymce"
 ]
 
 MIDDLEWARE = [
